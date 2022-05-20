b53ff23e55c36292cc89612d06e0b0cb652de088
Chansung Park <deep.diver.csp@gmail.com>
2022-05-20 14:29:15 +0000

    change model to resnet50

--- pipeline/models/model.py
+++ pipeline/models/model.py
@@ -12,10 +12,6 @@ from tfx.components.trainer.rewriting import rewriter_factory
 from tfx.dsl.io import fileio
 from tfx_bsl.tfxio import dataset_options
 
-import flatbuffers
-from tflite_support import metadata_schema_py_generated as _metadata_fb
-from tflite_support import metadata as _metadata
-
 from models import features
 
 _TRAIN_DATA_SIZE = 128
@@ -29,20 +25,10 @@ _CLASSIFIER_EPOCHS = 30
 _IMAGE_KEY = 'image'
 _LABEL_KEY = 'label'
 
-_TFLITE_MODEL_NAME = 'tflite'
-
 def _get_serve_image_fn(model):
   """Returns a function that feeds the input tensor into the model."""
-
   @tf.function
   def serve_image_fn(image_tensor):
-    """Returns the output to be used in the serving signature.
-    Args:
-      image_tensor: A tensor represeting input image. The image should have 3
-        channels.
-    Returns:
-      The model's predicton on input image tensor
-    """
     return model(image_tensor)
 
   return serve_image_fn
@@ -97,66 +83,11 @@ def _input_fn(file_pattern: List[str],
       dataset_options.TensorFlowDatasetOptions(
           batch_size=batch_size, label_key=features.transformed_name(_LABEL_KEY)),
       tf_transform_output.transformed_metadata.schema)
-  # Apply data augmentation. We have to do data augmentation here because
-  # we need to apply data agumentation on-the-fly during training. If we put
-  # it in Transform, it will only be applied once on the whole dataset, which
-  # will lose the point of data augmentation.
+
   if is_train:
     dataset = dataset.map(lambda x, y: (_data_augmentation(x), y))
 
   return dataset
-
-def _write_metadata(model_path: str, label_map_path: str, mean: List[float],
-                    std: List[float]):
-  """Add normalization option and label map TFLite metadata to the model.
-  Args:
-    model_path: The path of the TFLite model
-    label_map_path: The path of the label map file
-    mean: The mean value used to normalize input image tensor
-    std: The standard deviation used to normalize input image tensor
-  """
-
-  # Creates flatbuffer for model information.
-  model_meta = _metadata_fb.ModelMetadataT()
-
-  # Creates flatbuffer for model input metadata.
-  # Here we add the input normalization info to input metadata.
-  input_meta = _metadata_fb.TensorMetadataT()
-  input_normalization = _metadata_fb.ProcessUnitT()
-  input_normalization.optionsType = (
-      _metadata_fb.ProcessUnitOptions.NormalizationOptions)
-  input_normalization.options = _metadata_fb.NormalizationOptionsT()
-  input_normalization.options.mean = mean
-  input_normalization.options.std = std
-  input_meta.processUnits = [input_normalization]
-
-  # Creates flatbuffer for model output metadata.
-  # Here we add label file to output metadata.
-  output_meta = _metadata_fb.TensorMetadataT()
-  label_file = _metadata_fb.AssociatedFileT()
-  label_file.name = os.path.basename(label_map_path)
-  label_file.type = _metadata_fb.AssociatedFileType.TENSOR_AXIS_LABELS
-  output_meta.associatedFiles = [label_file]
-
-  # Creates subgraph to contain input and output information,
-  # and add subgraph to the model information.
-  subgraph = _metadata_fb.SubGraphMetadataT()
-  subgraph.inputTensorMetadata = [input_meta]
-  subgraph.outputTensorMetadata = [output_meta]
-  model_meta.subgraphMetadata = [subgraph]
-
-  # Serialize the model metadata buffer we created above using flatbuffer
-  # builder.
-  b = flatbuffers.Builder(0)
-  b.Finish(
-      model_meta.Pack(b), _metadata.MetadataPopulator.METADATA_FILE_IDENTIFIER)
-  metadata_buf = b.Output()
-
-  # Populates metadata and label file to the model file.
-  populator = _metadata.MetadataPopulator.with_model_file(model_path)
-  populator.load_metadata_buffer(metadata_buf)
-  populator.load_associated_files([label_map_path])
-  populator.populate()
 
 def _freeze_model_by_percentage(model: tf.keras.Model, percentage: float):
   """Freeze part of the model based on specified percentage.
@@ -182,11 +113,11 @@ def _freeze_model_by_percentage(model: tf.keras.Model, percentage: float):
       layer.trainable = True
 
 def _build_keras_model() -> tf.keras.Model:
-  base_model = tf.keras.applications.MobileNet(
+  base_model = tf.keras.applications.ResNet50(
       input_shape=(224, 224, 3),
       include_top=False,
       weights='imagenet',
-      pooling='avg')
+      pooling='max')
   base_model.input_spec = None
 
   # We add a Dropout layer at the top of MobileNet backbone we just created to
@@ -211,12 +142,6 @@ def _build_keras_model() -> tf.keras.Model:
   return model, base_model
 
 def run_fn(fn_args: FnArgs):
-  """Train the model based on given args.
-  Args:
-    fn_args: Holds args used to train the model as name/value pairs.
-  Raises:
-    ValueError: if invalid inputs.
-  """
   tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
 
   train_dataset = _input_fn(
@@ -225,6 +150,7 @@ def run_fn(fn_args: FnArgs):
       tf_transform_output,
       is_train=True,
       batch_size=_TRAIN_BATCH_SIZE)
+
   eval_dataset = _input_fn(
       fn_args.eval_files,
       fn_args.data_accessor,
@@ -257,7 +183,6 @@ def run_fn(fn_args: FnArgs):
       callbacks=[tensorboard_callback])
 
   absl.logging.info('Start fine-tuning the model')
-  # Unfreeze the top MobileNet layers and do joint fine-tuning
   _freeze_model_by_percentage(base_model, 0.9)
 
   # We need to recompile the model because layer properties have changed
@@ -288,25 +213,5 @@ def run_fn(fn_args: FnArgs):
 
   temp_saving_model_dir = os.path.join(fn_args.serving_model_dir, 'temp')
   model.save(temp_saving_model_dir, save_format='tf', signatures=signatures)
-
-  tfrw = rewriter_factory.create_rewriter(
-      rewriter_factory.TFLITE_REWRITER,
-      name='tflite_rewriter')
-  converters.rewrite_saved_model(temp_saving_model_dir,
-                                 fn_args.serving_model_dir, tfrw,
-                                 rewriter.ModelType.TFLITE_MODEL)
-
-  # Add necessary TFLite metadata to the model in order to use it within MLKit
-  # TODO(dzats@): Handle label map file path more properly, currently
-  # hard-coded.
-  tflite_model_path = os.path.join(fn_args.serving_model_dir,
-                                   _TFLITE_MODEL_NAME)
-  # TODO(dzats@): Extend the TFLite rewriter to be able to add TFLite metadata
-  #@ to the model.
-  _write_metadata(
-      model_path=tflite_model_path,
-      label_map_path=fn_args.custom_config['labels_path'],
-      mean=[127.5],
-      std=[127.5])
 
   fileio.rmtree(temp_saving_model_dir)
--- pipeline/models/preprocessing.py
+++ pipeline/models/preprocessing.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.applications import resnet50
 from models import features
 
 def preprocessing_fn(inputs):
@@ -15,8 +16,7 @@ def preprocessing_fn(inputs):
       inputs[features.IMAGE_KEY],
       fn_output_signature=(tf.uint8))
   image_features = tf.image.resize(image_features, [224, 224])  
-  image_features = tf.keras.applications.mobilenet.preprocess_input(
-      image_features)
+  image_features = resnet50.preprocess_input(image_features)
 
   outputs[features.transformed_name(features.IMAGE_KEY)] = image_features
   outputs[features.transformed_name(features.LABEL_KEY)] = inputs[features.LABEL_KEY]
