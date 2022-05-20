c3305a58998c65dbaf322b26c4c061705506d010
Chansung Park <deep.diver.csp@gmail.com>
2022-05-20 14:47:50 +0900

    Update pipeline.py

--- pipeline/pipeline/pipeline.py
+++ pipeline/pipeline/pipeline.py
@@ -1,21 +1,3 @@
-# Copyright 2020 Google LLC. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""TFX penguin template pipeline definition.
-
-This file defines TFX pipeline and various components in the pipeline.
-"""
-
 from typing import List, Optional
 
 import tensorflow_model_analysis as tfma
@@ -24,7 +6,6 @@ from models import features
 
 from ml_metadata.proto import metadata_store_pb2
 from tfx.proto import example_gen_pb2
-
 
 def create_pipeline(
     pipeline_name: str,
@@ -41,13 +22,8 @@ def create_pipeline(
         metadata_store_pb2.ConnectionConfig] = None,
     beam_pipeline_args: Optional[List[str]] = None,
 ) -> tfx.dsl.Pipeline:
-  """Implements the penguin pipeline with TFX."""
-
   components = []
 
-  # Brings data into the pipeline or otherwise joins/converts training data.
-  # TODO(step 2): Might use another ExampleGen class for your data.
-#   example_gen = tfx.components.CsvExampleGen(input_base=data_path)
   input_config = example_gen_pb2.Input(splits=[
       example_gen_pb2.Input.Split(name='train', pattern='train/*'),
       example_gen_pb2.Input.Split(name='eval', pattern='test/*')
@@ -55,46 +31,36 @@ def create_pipeline(
   example_gen = tfx.components.ImportExampleGen(input_base=data_path, input_config=input_config)
   components.append(example_gen)
 
-  # Computes statistics over data for visualization and example validation.
   statistics_gen = tfx.components.StatisticsGen(
       examples=example_gen.outputs['examples'])
   components.append(statistics_gen)
 
   if schema_path is None:
-    # Generates schema based on statistics files.
     schema_gen = tfx.components.SchemaGen(
         statistics=statistics_gen.outputs['statistics'])
     components.append(schema_gen)
   else:
-    # Import user provided schema into the pipeline.
     schema_gen = tfx.components.ImportSchemaGen(schema_file=schema_path)
     components.append(schema_gen)
 
-    # Performs anomaly detection based on statistics and data schema.
-    example_validator = tfx.components.ExampleValidator(  # pylint: disable=unused-variable
-        statistics=statistics_gen.outputs['statistics'],
-        schema=schema_gen.outputs['schema'])
-    # components.append(example_validator)
+#   example_validator = tfx.components.ExampleValidator(  
+#       statistics=statistics_gen.outputs['statistics'],
+#       schema=schema_gen.outputs['schema'])
+#   components.append(example_validator)
 
-  # Performs transformations and feature engineering in training and serving.
-  transform = tfx.components.Transform(  # pylint: disable=unused-variable
+  transform = tfx.components.Transform(  
       examples=example_gen.outputs['examples'],
       schema=schema_gen.outputs['schema'],
       preprocessing_fn=preprocessing_fn)
-  # TODO(step 3): Uncomment here to add Transform to the pipeline.
   components.append(transform)
 
-  # Uses user-provided Python function that implements a model using Tensorflow.
   trainer = tfx.components.Trainer(
       run_fn=run_fn,
-    #   examples=example_gen.outputs['examples'],
-      # Use outputs of Transform as training inputs if Transform is used.
       examples=transform.outputs['transformed_examples'],
       transform_graph=transform.outputs['transform_graph'],
       schema=schema_gen.outputs['schema'],
       train_args=train_args,
       eval_args=eval_args)
-  # TODO(step 4): Uncomment here to add Trainer to the pipeline.
   components.append(trainer)
 
   # Get the latest blessed model for model validation.
