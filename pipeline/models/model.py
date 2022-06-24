4ae7641330af65929330f3397da8abba1020c660
Chansung Park <deep.diver.csp@gmail.com>
2022-06-24 17:31:19 +0000

    remove tflite module imports

--- pipeline/models/model.py
+++ pipeline/models/model.py
@@ -7,9 +7,6 @@ import tensorflow_transform as tft
 
 from tfx.components.trainer.fn_args_utils import DataAccessor
 from tfx.components.trainer.fn_args_utils import FnArgs
-from tfx.components.trainer.rewriting import converters
-from tfx.components.trainer.rewriting import rewriter
-from tfx.components.trainer.rewriting import rewriter_factory
 from tfx.dsl.io import fileio
 from tfx_bsl.tfxio import dataset_options
 
