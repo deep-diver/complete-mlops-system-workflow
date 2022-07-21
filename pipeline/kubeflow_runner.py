3681d8e5b5375eced84e78d34e42ff1c73407dd1
Chansung Park <deep.diver.csp@gmail.com>
2022-07-21 03:13:01 +0000

    clean up

--- pipeline/kubeflow_runner.py
+++ pipeline/kubeflow_runner.py
@@ -2,34 +2,14 @@ import os
 from absl import logging
 
 from tfx import v1 as tfx
+from tfx.proto import TrainArgs, EvalArgs
 from tfx.orchestration.kubeflow.v2 import kubeflow_v2_dag_runner
 from pipeline import configs
 from pipeline import pipeline
 
-# TFX pipeline produces many output files and metadata. All output data will be
-# stored under this OUTPUT_DIR.
 OUTPUT_DIR = os.path.join('gs://', configs.GCS_BUCKET_NAME)
-
-# TFX produces two types of outputs, files and metadata.
-# - Files will be created under PIPELINE_ROOT directory.
-PIPELINE_ROOT = os.path.join(OUTPUT_DIR, 'tfx_pipeline_output',
-                             configs.PIPELINE_NAME)
-
-# The last component of the pipeline, "Pusher" will produce serving model under
-# SERVING_MODEL_DIR.
+PIPELINE_ROOT = os.path.join(OUTPUT_DIR, 'tfx_pipeline_output', configs.PIPELINE_NAME)
 SERVING_MODEL_DIR = os.path.join(PIPELINE_ROOT, 'serving_model')
-
-# Specifies data file directory. DATA_PATH should be a directory containing CSV
-# files for CsvExampleGen in this example. By default, data files are in the
-# GCS path: `gs://{GCS_BUCKET_NAME}/tfx-template/data/`. Using a GCS path is
-# recommended for KFP.
-#
-# One can optionally choose to use a data source located inside of the container
-# built by the template, by specifying
-# DATA_PATH = 'data'. Note that Dataflow does not support use container as a
-# dependency currently, so this means CsvExampleGen cannot be used with Dataflow
-# (step 8 in the template notebook).
-
 DATA_PATH = 'gs://{}/data/'.format(configs.GCS_BUCKET_NAME)
 
 def run():
@@ -43,14 +23,16 @@ def run():
           pipeline_name=configs.PIPELINE_NAME,
           pipeline_root=PIPELINE_ROOT,
           data_path=DATA_PATH,
-          # NOTE: Set the path of the customized schema if any.
-          # schema_path=generated_schema_path,
           preprocessing_fn=configs.PREPROCESSING_FN,
           run_fn=configs.RUN_FN,
-          train_args=tfx.proto.TrainArgs(num_steps=configs.TRAIN_NUM_STEPS),
+          train_args=TrainArgs(
+            num_steps=configs.TRAIN_NUM_STEPS
+          ),
           train_cloud_region='us-central1',
           train_cloud_args=configs.TRAINING_JOB_SPEC,
-          eval_args=tfx.proto.EvalArgs(num_steps=configs.EVAL_NUM_STEPS),
+          eval_args=EvalArgs(
+            num_steps=configs.EVAL_NUM_STEPS
+          ),
           eval_accuracy_threshold=configs.EVAL_ACCURACY_THRESHOLD,
           serving_model_dir=SERVING_MODEL_DIR,
       ))
