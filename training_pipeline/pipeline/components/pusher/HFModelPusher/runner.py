8208f9ca408ebe97314a84210f27e852f4ec3bdc
Chansung Park <deep.diver.csp@gmail.com>
2022-08-17 13:05:11 +0000

    add nested try

--- training_pipeline/pipeline/components/pusher/HFModelPusher/runner.py
+++ training_pipeline/pipeline/components/pusher/HFModelPusher/runner.py
@@ -30,50 +30,54 @@ def release_model_for_hf_model(
     hf_api.set_access_token(access_token)
 
     logging.warning(f"model_path: {model_path}")
-    if model_path.startswith("gs://"):
-        logging.warning("download pushed model")
-        root_dir = "saved_model"
-        os.mkdir(root_dir)
 
-        blobnames = tf.io.gfile.listdir(model_path)
-
-        for blobname in blobnames:
-            blob = f"{model_path}/{blobname}"
-
-            if tf.io.gfile.isdir(blob):
-                sub_dir = f"{root_dir}/{blobname}"
-                os.mkdir(sub_dir)
-
-                sub_blobnames = tf.io.gfile.listdir(blob)
-                for sub_blobname in sub_blobnames:
-                    sub_blob = f"{blob}{sub_blobname}"
-
-                    logging.warning(f"{sub_dir}/{sub_blobname}")
-                    tf.io.gfile.copy(sub_blob, f"{sub_dir}{sub_blobname}")
-            else:
-                logging.warning(f"{root_dir}/{blobname}")
-                tf.io.gfile.copy(blob, f"{root_dir}/{blobname}")
-
-        model_path = root_dir
-
+    logging.warning("download pushed model")
     model_name = f"v{int(time.time())}"
+    root_dir = model_name
+    os.mkdir(root_dir)
 
+    blobnames = tf.io.gfile.listdir(model_path)
+
+    for blobname in blobnames:
+        blob = f"{model_path}/{blobname}"
+
+        if tf.io.gfile.isdir(blob):
+            sub_dir = f"{root_dir}/{blobname}"
+            os.mkdir(sub_dir)
+
+            sub_blobnames = tf.io.gfile.listdir(blob)
+            for sub_blobname in sub_blobnames:
+                sub_blob = f"{blob}{sub_blobname}"
+
+                logging.warning(f"{sub_dir}/{sub_blobname}")
+                tf.io.gfile.copy(sub_blob, f"{sub_dir}{sub_blobname}")
+        else:
+            logging.warning(f"{root_dir}/{blobname}")
+            tf.io.gfile.copy(blob, f"{root_dir}/{blobname}")
+
+    model_path = root_dir
+
+    hf_hub_path = ""
     try:
         hf_api.create_repo(
             token=access_token, repo_id=f"{repo_id}-model", repo_type=repo_type
         )
-    except HTTPError:
-        logging.warning(f"{repo_id}-model repository already exists")
-
-    hf_hub_path = hf_api.upload_folder(
-        repo_id=f"{repo_id}-model",        
-        folder_path=model_path,
-        path_in_repo=f"/checkpoints/{model_name}",
-        token=access_token,
-        create_pr=True,
-        repo_type=repo_type,
-        commit_message=model_name,
-    )
-    logging.warning(f"file is uploaded at {repo_id}-model")
+    except HTTPError as e:
+        logging.warning(e)
+        logging.warning(f"{repo_id}-model repository may already exist")
+    finally:
+        try:
+            hf_hub_path = hf_api.upload_folder(
+                repo_id=f"{repo_id}-model",
+                folder_path=model_path,
+                token=access_token,
+                create_pr=True,
+                repo_type=repo_type,
+                commit_message=model_name,
+            )
+            logging.warning(f"file is uploaded at {repo_id}-model")
+        except HTTPError:
+            logging.warning(e)
+            raise HTTPError
 
     return hf_hub_path
