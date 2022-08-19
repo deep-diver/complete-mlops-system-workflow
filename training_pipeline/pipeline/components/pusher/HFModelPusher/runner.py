2e89accab27f7e378f3372a4c9abbe77695e3504
Chansung Park <deep.diver.csp@gmail.com>
2022-08-19 04:12:28 +0000

    remove hfapi.upload_folder

--- training_pipeline/pipeline/components/pusher/HFModelPusher/runner.py
+++ training_pipeline/pipeline/components/pusher/HFModelPusher/runner.py
@@ -31,11 +31,20 @@ def release_model_for_hf_model(
     repo_url = f"{repo_url_prefix}/{repo_id}"
 
     logging.warning(f"model_path: {model_path}")
-
     logging.warning("download pushed model")
-    root_dir = model_version_name
-    os.mkdir(root_dir)
 
+    try:
+        HfApi().create_repo(token=access_token, repo_id=repo_id, repo_type=repo_type)
+    except HTTPError:
+        logging.warning(f"{repo_id}-model repository may already exist")
+        logging.warning("this is expected behaviour if you overwrite with a new branch")
+
+    repository = Repository(
+        local_dir="hf-model-repo/", clone_from=repo_url, use_auth_token=access_token
+    )
+    repository.git_checkout(revision=model_version_name, create_branch_ok=True)
+
+    root_dir = "hf-model-repo"
     blobnames = tf.io.gfile.listdir(model_path)
 
     for blobname in blobnames:
@@ -43,52 +52,27 @@ def release_model_for_hf_model(
 
         if tf.io.gfile.isdir(blob):
             sub_dir = f"{root_dir}/{blobname}"
-            os.mkdir(sub_dir)
+
+            try:
+                os.mkdir(sub_dir)
+            except FileExistsError as error:
+                logging.warning(
+                    f"folder {sub_dir} already exists, this is expected behaviour if you overwrite with a new branch"
+                )
+                logging.warning(error)
 
             sub_blobnames = tf.io.gfile.listdir(blob)
             for sub_blobname in sub_blobnames:
                 sub_blob = f"{blob}{sub_blobname}"
 
                 logging.warning(f"{sub_dir}/{sub_blobname}")
-                tf.io.gfile.copy(sub_blob, f"{sub_dir}{sub_blobname}")
+                tf.io.gfile.copy(sub_blob, f"{sub_dir}{sub_blobname}", overwrite=True)
         else:
             logging.warning(f"{root_dir}/{blobname}")
-            tf.io.gfile.copy(blob, f"{root_dir}/{blobname}")
-
-    model_path = root_dir
-
-    try:
-        HfApi().create_repo(token=access_token, repo_id=repo_id, repo_type=repo_type)
-    except HTTPError:
-        logging.warning(f"{repo_id}-model repository may already exist")
-        pass
-
-    repository = Repository(
-        local_dir="hf-model-repo/", clone_from=repo_url, use_auth_token=access_token
-    )
-
-    repository.git_checkout(revision=model_version_name, create_branch_ok=True)
-
-    dummy_filepath = f"hf-model-repo/{model_version_name}"
-    open(dummy_filepath, mode="w", encoding="utf-8").close()
+            tf.io.gfile.copy(blob, f"{root_dir}/{blobname}", overwrite=True)
 
     repository.git_add(pattern=".")
-    repository.git_commit(commit_message="add dummy data to create branch")
+    repository.git_commit(commit_message="updload new version of the model")
     repository.git_push(upstream=f"origin {model_version_name}")
-
-    try:
-        HfApi().upload_folder(
-            repo_id=repo_id,
-            folder_path=model_path,
-            path_in_repo=".",
-            token=access_token,
-            create_pr=True,
-            repo_type=repo_type,
-            commit_message=model_version_name,
-            revision=model_version_name,
-        )
-        logging.warning(f"file is uploaded at {repo_id}")
-    except HTTPError as error:
-        logging.warning(error)
 
     return (repo_id, repo_url)
