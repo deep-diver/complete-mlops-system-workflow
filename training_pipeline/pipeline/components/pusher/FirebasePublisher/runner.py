fc7b5d9117b4c817a349f46fe032c68253219810
Chansung Park <deep.diver.csp@gmail.com>
2022-08-21 06:36:11 +0000

    initial impl done

--- training_pipeline/pipeline/components/pusher/FirebasePublisher/runner.py
+++ training_pipeline/pipeline/components/pusher/FirebasePublisher/runner.py
@@ -1,84 +1,101 @@
-from typing import Any, Dict
-
-import os
-import tarfile
+from typing import Any, Dict, Tuple
 from absl import logging
 
-from github import Github
 import tensorflow as tf
+
+import firebase_admin
+from firebase_admin import ml
+from firebase_admin import storage
+from firebase_admin import credentials
 
 from pipeline.components.pusher.FirebasePublisher import constants
 
 
 def deploy_model_for_firebase_ml(
     model_version_name: str,
-    serving_path: str,
+    model_path: str,
     firebase_ml_args: Dict[str, Any],
 ):
-    gcs_bucket = firebase_ml_args[constants.FIREBASE_GCS_BUCKET_KEY]
+    model_name = firebase_ml_args[constants.FIREBASE_ML_MODEL_NAME_KEY]
+    firebase_gcs_bucket = firebase_ml_args[constants.FIREBASE_GCS_BUCKET_KEY]
+    tags = firebase_ml_args[constants.FIREBASE_ML_MODEL_TAGS_KEY]
+    tags.append(model_version_name)
 
-    # model_uri = f"{pushed_model.uri}/model.tflite"
+    is_tfile, model_path = _download_pushed_model(model_path, "temp_model")
 
-    # assert model_uri.split("://")[0] == "gs"
-    # assert credential_uri.split("://")[0] == "gs"
-
-    # # create gcs client instance
-    # gcs_client = gcs_storage.Client()
-
-    # # get credential for firebase
-    # credential_gcs_bucket = credential_uri.split("//")[1].split("/")[0]
-    # credential_blob_path = "/".join(credential_uri.split("//")[1].split("/")[1:])
-
-    # bucket = gcs_client.bucket(credential_gcs_bucket)
-    # blob = bucket.blob(credential_blob_path)
-    # blob.download_to_filename("credential.json")
-    # logging.info(f"download credential.json from {credential_uri} is completed")
-
-    # # get tflite model file
-    # tflite_gcs_bucket = model_uri.split("//")[1].split("/")[0]
-    # tflite_blob_path = "/".join(model_uri.split("//")[1].split("/")[1:])
-
-    # bucket = gcs_client.bucket(tflite_gcs_bucket)
-    # blob = bucket.blob(tflite_blob_path)
-    # blob.download_to_filename("model.tflite")
-    # logging.info(f"download model.tflite from {model_uri} is completed")
-
+    firebase_admin.initialize_app()
+    logging.info("firebase_admin initialize app is completed")
     # firebase_admin.initialize_app(
     #     credentials.Certificate("credential.json"),
     #     options={"storageBucket": firebase_dest_gcs_bucket},
     # )
     # logging.info("firebase_admin initialize app is completed")
 
-    # model_list = ml.list_models(list_filter=f"display_name={model_display_name}")
-    # # update
-    # if len(model_list.models) > 0:
-    #     # get the first match model
-    #     model = model_list.models[0]
-    #     source = ml.TFLiteGCSModelSource.from_tflite_model_file("model.tflite")
-    #     model.model_format = ml.TFLiteFormat(model_source=source)
+    if is_tfile:
+        source = ml.TFLiteGCSModelSource.from_tflite_model_file(model_path)
+    else:
+        source = ml.TFLiteGCSModelSource.from_saved_model(model_path)
 
-    #     updated_model = ml.update_model(model)
-    #     ml.publish_model(updated_model.model_id)
+    model_list = ml.list_models(list_filter=f"display_name={model_name}")
+    # update
+    if len(model_list.models) > 0:
+        # get the first match model
+        model = model_list.models[0]
+        model.tags = tags
+        model.model_format = ml.TFLiteFormat(model_source=source)
 
-    #     logging.info("model exists, so update it in FireBase ML")
-    #     return {"result": "model updated"}
-    # # create
-    # else:
-    #     # load a tflite file and upload it to Cloud Storage
-    #     source = ml.TFLiteGCSModelSource.from_tflite_model_file("model.tflite")
+        updated_model = ml.update_model(model)
+        ml.publish_model(updated_model.model_id)
 
-    #     # create the model object
-    #     tflite_format = ml.TFLiteFormat(model_source=source)
-    #     model = ml.Model(
-    #         display_name=model_display_name,
-    #         tags=[model_tag],
-    #         model_format=tflite_format,
-    #     )
+        logging.info("model exists, so update it in FireBase ML")
+    # create
+    else:
+        # create the model object
+        tflite_format = ml.TFLiteFormat(model_source=source)
+        model = ml.Model(
+            display_name=model_name,
+            tags=tags,
+            model_format=tflite_format,
+        )
 
-    #     # Add the model to your Firebase project and publish it
-    #     new_model = ml.create_model(model)
-    #     ml.publish_model(new_model.model_id)
+        # Add the model to your Firebase project and publish it
+        new_model = ml.create_model(model)
+        ml.publish_model(new_model.model_id)
 
-    #     logging.info("model doesn exists, so create one in FireBase ML")
-    #     return {"result": "model created"}
+        logging.info("model doesn exists, so create one in FireBase ML")
     return ""
+
+
+def _download_pushed_model(model_path: str, destination_path: str) -> Tuple[bool, str]:
+    is_tfile = False
+
+    logging.warning("download pushed model")
+    root_dir = destination_path  # "saved_model"
+
+    if not tf.io.gfile.exists(root_dir):
+        tf.io.gfile.mkdir(root_dir)
+
+    blobnames = tf.io.gfile.listdir(model_path)
+
+    for blobname in blobnames:
+        blob = f"{model_path}/{blobname}"
+
+        if tf.io.gfile.isdir(blob):
+            sub_dir = f"{root_dir}/{blobname}"
+            tf.io.gfile.mkdir(sub_dir)
+
+            sub_blobnames = tf.io.gfile.listdir(blob)
+            for sub_blobname in sub_blobnames:
+                sub_blob = f"{blob}{sub_blobname}"
+
+                logging.warning(f"{sub_dir}/{sub_blobname}")
+                tf.io.gfile.copy(sub_blob, f"{sub_dir}{sub_blobname}")
+        else:
+            logging.warning(f"{root_dir}/{blobname}")
+            tf.io.gfile.copy(blob, f"{root_dir}/{blobname}")
+
+            if "tflite" in blobname:
+                is_tfile = True
+                model_path = f"{root_dir}/{blobname}"
+
+    return (is_tfile, model_path)
